9a27d9c0336de8770dd94a1e301dbf8c87eb014e
RuelSammy <ruelmunyalo@gmail.com>
2025-02-04 18:42:32 +0300

    day1

---
+++ day1.py
@@ -0,0 +1,31 @@
+print("Hello World")
+#reminding myself of single line comments
+'''
+Reminding myself of multi-line comments
+These can be declared using three ' or "
+'''
+"""
+PYTHON Questions
+1. How is it compiled?
+Python is an interpreted language. Not a compiled one(Although there is some compilation going on)
+Python code is intepreted line by line as byte code and then into machine language(1s and 0s) 
+when you write a Python program, you’re essentially writing instructions in a high-level language
+that you understand. This code is then compiled into byte code, an intermediary step to make execution 
+easier. The Python interpreter reads the byte code, line by line, and acts on the instructions. 
+Finally, the byte code is translated into machine language (0s and 1s) by lower-level system components, 
+which is read by the computer’s transistors.
+
+2. Python vs Java
+a) Compilation vs Interpretation - Java is a compiled language while python is an interpreted language
+b) Syntax - Java has a simpler syntax relative to C thus appearing easier. Python however doesn't have the requirements
+            to have curly braces and closely resembles English making it easier to learn.
+c) Performance - both Java and Python compile into bytecode and run on virtual machines allowing them to be 
+                cross-platform python however is slower than Java.
+d) Stability - Java is considered more stable than python. 
+e) Security - Python edges out Java in terms of security.
+
+3. Compiler vs Interpreter - compilers and interpreters both serve the purpose of converting high-level code into
+ something a computer can understand, but they do so in different ways. A compiler translates the whole program at 
+ once, which can make it run faster but takes more time to compile. An interpreter translates and runs the code 
+ line by line, making it easier to catch errors and debug, though it may run slower.
+"""
