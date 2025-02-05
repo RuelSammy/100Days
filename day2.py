d48c88905479b5e240a3e947f5c9c94175e5353a
RuelSammy <ruelmunyalo@gmail.com>
2025-02-05 22:55:25 +0300

    day 2/100

---
+++ day2.py
@@ -0,0 +1,44 @@
+name = "John Doe" #this is a variable called a string
+age = 34 #this is an integer
+height = 6.2 #this is a floating point number
+is_student = False #this is a boolean
+email = None  #this represents a null value
+siblings = ['Jane' 'Andrew' 'Agnes']  #this is a list. It is mutable
+address = {"street": "Baker Street","city": "London"} #this is a dictionary key:value is the syntax
+
+''''
+terms: initialization, declaration and assignment of variables
+Initialisation - refers to the assignment of an initial value for a variable or data object.
+Declaration - a statement that specifies the properties of an identifier, such as a variable, function, 
+            or constant
+Assignment -  a statement that stores a value in a variable
+Declaration – This is when you tell the compiler or interpreter that a variable exists, but you don’t necessarily give it a value yet.
+c
+int x;  // Declaring an integer variable named x
+python
+x = None  # Declaring a variable in Python (not required explicitly)
+Assignment – This is when you give a value to a variable after it has been declared.
+c
+x = 10;  // Assigning 10 to x
+python
+x = 10  # Assigning 10 to x in Python
+Initialization – This is when you declare and assign a value to a variable at the same time.
+
+c
+int y = 20;  // Declaration and initialization in C
+python
+y = 20  # Declaration and initialization in Python
+
+
+difference between Dynamically typed and Statically typed variables
+A dynamically typed language is a programming language that determines 
+a variable's type at runtime, based on the variable's value. This means 
+that the language doesn't require the programmer to specify the type of 
+a variable before using it.
+e.g - JavaScript, Python, Ruby, Perl, Objective-C, PHP, Lisp, and Tcl.
+
+A statically typed language is a programming language that checks 
+variable types during compilation, before the code runs. This allows 
+for earlier error detection and more efficient code. 
+e.g - Java, C,C++,Fortran,COBOL
+ '''
