5f1733fdb3f7869eb1489f298e1cb7d5cfa18323
RuelSammy <ruelmunyalo@gmail.com>
2025-02-07 21:11:43 +0300

    day 4/100

---
+++ day4.py
@@ -0,0 +1,36 @@
+num1 = 10
+num2 = 3
+
+print("\n ___Arithmetic Operations___")
+print("Addition (10+3):", num1+num2)
+print("Subtraction (10-3):", num1-num2)
+print("Multiplication (10*3):", num1*num2)
+print("Division (10/3):", num1/num2)
+print("Floor division (10//3:)", num1//num2)
+print("Modulus (10%3:)", num1%num2)
+print("Exponential (10**3)", num1**num2)
+
+print("\n ___Relational Operations___")
+print("Is 10 equal to 3?", num1 == num2)
+print("Is 10 not equal to 3?", num1 != num2)
+print("Is 10 greater than 3?", num1 > num2)
+print("Is 10 lesser than 3?", num1 < num2)
+print("Is 10 greater than or equal to 3?", num1 >= num2)
+print("Is 10 lesser than or equal to 3?", num1 <= num2)
+
+true_value = True
+false_value = False
+
+print("\n___Logical Operations___")
+print("True and False", true_value and false_value)
+print("True and True", true_value and true_value)
+print("False and True", false_value and true_value)
+print("False and False", false_value and false_value)
+print("True or False", true_value or false_value)
+print("True or True", true_value or true_value)
+print("False or True", false_value or true_value)
+print("False or False", false_value or false_value)
+print("NOT True", not true_value)
+print("NOT false", not false_value)
+
+      
