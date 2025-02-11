3ce41d886b332817f61fa5b349dd80ec38c1b9de
RuelSammy <ruelmunyalo@gmail.com>
2025-02-11 20:14:29 +0300

    day 8/100

---
+++ day8.py
@@ -0,0 +1,31 @@
+#Calculator app
+#Addition
+def add_numbers(num1,num2):
+    return num1 + num2
+
+addition = add_numbers(3,5)
+print (addition)
+
+#Subtraction
+def subtract_numbers(num1,num2):
+    return num1-num2
+
+subtraction = subtract_numbers(62,20)
+print(subtraction)
+
+#Multiplication
+def multiply_numbers(num1,num2):
+    return num1*num2
+
+multiplication = multiply_numbers(4,5)
+print(multiplication)
+
+#Division
+def divide_numbers(num1,num2):
+    return num1/num2
+
+division = divide_numbers(80,10)
+print(division)
+
+
+
