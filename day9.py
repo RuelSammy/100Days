ca23cab149d732d273b55afb071a64f32d87460b
RuelSammy <ruelmunyalo@gmail.com>
2025-02-12 22:36:37 +0300

    Day 9

---
+++ day9.py
@@ -0,0 +1,8 @@
+#Program that generates a random number
+import random
+num = random.random()
+print(num)
+#program that generates a random number within a specified range
+import random
+print(random.randint(0,9))
+
