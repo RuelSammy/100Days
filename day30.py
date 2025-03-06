275b8ccbeccd1908b0539794006e59ce891f54bd
RuelSammy <ruelmunyalo@gmail.com>
2025-03-06 20:57:01 +0300

    Day31

---
+++ day30.py
@@ -0,0 +1,6 @@
+#Code to Sort a list of numbers in ascending order.
+a = [5, 2, 9, 1, 5, 6]
+
+# Sort the value in increasing order
+a.sort()
+print(a)
---
+++ day31.py
@@ -0,0 +1,6 @@
+#Code to merge two dictionaries
+d1 = {'x': 1, 'y': 2}
+d2 = {'y': 3, 'z': 4}
+
+d3 = d1 | d2
+print(d3)
