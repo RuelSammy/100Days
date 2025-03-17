0b030464d4ccb63657469b64fdf5804d798416f6
RuelSammy <ruelmunyalo@gmail.com>
2025-03-17 19:19:41 +0300

    Day 41

---
+++ day41.py
@@ -0,0 +1,17 @@
+#Implement inheritance between classes.
+# Parent class
+class Animal:
+    def __init__(self, name):
+        self.name = name  # Initialize the name attribute
+
+    def speak(self):
+        pass  # Placeholder method to be overridden by child classes
+
+# Child class inheriting from Animal
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} barks!"  # Override the speak method
+
+# Creating an instance of Dog
+dog = Dog("Buddy")
+print(dog.speak())  
