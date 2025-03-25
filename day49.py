c1749c7a43e56d0fcd98564c7009857b2df9b6a8
RuelSammy <ruelmunyalo@gmail.com>
2025-03-25 13:27:41 +0300

    Day 49

---
+++ day49.py
@@ -0,0 +1,9 @@
+#Implement a binary search tree.
+class Node:
+    def __init__(self, key):
+        self.value = key  
+        self.left = None  
+        self.right = None    
+
+# Creating the root node
+root = Node(5)
